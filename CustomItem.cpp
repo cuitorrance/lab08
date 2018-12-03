9026b07e8bc9a8d1d6de54596fd16a57ee59cc73
Torrance Cui <torrancecui@cs.ucsb.edu>
2018-12-02 23:20:38 -0800

    finished

--- CustomItem.cpp
+++ CustomItem.cpp
@@ -1,5 +1,7 @@
 #include "CustomItem.h"
 #include <math.h>
+#include <sstream>
+#include <iomanip>
 
 CustomItem::CustomItem(std::string size) : IceCreamItem::IceCreamItem(size){
   this->size = size;
@@ -21,15 +23,47 @@ CustomItem::~CustomItem(){
 }
 
 std::string CustomItem::composeItem(){
+
+  std::stringstream stream;
+  stream << std::fixed << std::setprecision(2) << getPrice();
   std::string ans = "Custom Size: " + size + "\n"
-    + "Toppings:" + "\n"
-    + "Price: $" +std::to_string(getPrice()) + "\n";
+    + "Toppings:" + "\n";
+
+  for (std::map<std::string, int>::iterator it = toppings.begin(); it!=toppings.end(); it++)
+    {
+      ans += it->first + ": " + std::to_string(it->second) + " oz" + "\n";
+    }
+    
+  ans += "Price: $" +stream.str() + "\n";
 
   return ans;
 }
-double CustomItem::getPrice(){
-  return (round(price*100))/100;
-}
+
 void CustomItem::addTopping(std::string topping){
   
+  //  for (std::map<std::string, int>::iterator it = toppings.begin();it !=toppings.end() ;it++)
+  //{
+  //  if ( it->first == topping)
+  //	{
+  //	  it->second++;
+  //	}
+  //  else
+  //	{
+  std::map<std::string, int>::iterator it =toppings.find(topping);
+  if (it != toppings.end())
+    {
+      toppings[topping]++;
+      price += 0.4;
+    }
+  else
+    {
+      toppings[topping] =1;
+      price += 0.4;
+    }
+	   //	}
+	   //}
+}
+double CustomItem::getPrice(){
+
+  return price;
 }
--- PreMadeItem.cpp
+++ PreMadeItem.cpp
@@ -1,6 +1,8 @@
 #include "PreMadeItem.h"
 #include <iomanip>
 #include <cmath>
+#include <sstream>
+#include <string>
 
 PreMadeItem::PreMadeItem(std::string name, std::string size): IceCreamItem::IceCreamItem(size){
   this->name = name;
@@ -21,12 +23,15 @@ PreMadeItem::PreMadeItem(std::string name, std::string size): IceCreamItem::IceC
 PreMadeItem::~PreMadeItem(){
 }
 std::string PreMadeItem:: composeItem(){
+
+  std::stringstream stream;
+  stream << std::fixed << std::setprecision(2) << getPrice();
+  
   std::string ans = "Pre-made Size: " + size + "\n"
              + "Pre-made Item: " + name + "\n"
-             + "Price: $" + std::to_string(getPrice()) + "\n";
+    + "Price: $" + stream.str() + "\n";
   return ans;
 }
-
 double PreMadeItem::getPrice(){
-  return std::floor((price*100)+0.5) /100;
+  return price;
 }
